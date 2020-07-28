420ee81b21727fcb49d5c1e9e19fbd6875e363e3
ferecgithub <ferechamitbeyli@gmail.com>
2020-07-28 11:47:45 +0300

    first upload

---
+++ app/src/main/java/com/ferechamitbeyli/workmanagerdemo1/DownloadingWorker.kt
@@ -0,0 +1,26 @@
+package com.ferechamitbeyli.workmanagerdemo1
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.lang.Exception
+
+
+class DownloadingWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+
+
+    override fun doWork(): Result {
+        try {
+
+            for(i in 0..3000) {
+                Log.i("MYTAG", "Downloading $i")
+            }
+
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+
+    }
+}
